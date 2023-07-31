8ff2447cf153dccea349f785f3b702db0ef23e72
Oğul Can <ogulcanogul@gmail.com>
2023-07-31 04:32:20 +0300

    cerebrate first changes

--- llmfoundry/models/layers/ffn.py
+++ llmfoundry/models/layers/ffn.py
@@ -95,7 +95,7 @@ class CerebrateMLP(nn.Module):
         x = self.up_proj(x)
         x = self.act(x)
         mean_activations = torch.mean(torch.mean(torch.abs(x), 0), 0)
-        mean_activations.to('meta')
+        mean_activations = mean_activations.to('meta')
         #self.neuron_activation = self.neuron_activation.to(mean_activations.device)
         self.neuron_activation = (self.decay_weight_ma  * self.neuron_activation) + ((1-self.decay_weight_ma) * mean_activations)
         #self.neuron_activation = self.neuron_activation.to('meta')
