7c22f6f6c1b869a98402f09e4571c68a9045ec2b
Oğul Can <ogulcanogul@gmail.com>
2023-07-31 03:52:19 +0300

    cerebrate first changes

--- llmfoundry/models/layers/ffn.py
+++ llmfoundry/models/layers/ffn.py
@@ -87,8 +87,8 @@ class CerebrateMLP(nn.Module):
             min(1, p_tohold - (p_tohold * max(0,
             (step-neuron_keep_steps)/(max_step_size-neuron_keep_steps)))+\
             neuron_keep_probability + neuron_keep_probability / math.exp(5 * step / max_step_size))
-        self.neuron_activation = torch.zeros(expansion_ratio * d_model,  **fc_kwargs)
-        self.neuron_mask = torch.ones(expansion_ratio * d_model,  **fc_kwargs)
+        self.neuron_activation = torch.zeros(expansion_ratio * d_model, device='meta')
+        self.neuron_mask = torch.ones(expansion_ratio * d_model,  device='meta')
         self.decay_weight_ma = decay_weight_ma
 
     def forward(self, x):
