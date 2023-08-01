3ba1a19f47f970b662532fa335e5bc2ca059eb72
90530 <ogulcanogul@gmail.com>
2023-08-01 15:27:16 +0300

    cerebrate first changes

--- llmfoundry/models/layers/ffn.py
+++ llmfoundry/models/layers/ffn.py
@@ -96,7 +96,7 @@ class CerebrateMLP(nn.Module):
         x = self.up_proj(x)
         x = self.act(x)
 
-        if self.training:
+        if self.training == True:
             mean_activations = torch.mean(torch.mean(torch.abs(x), 0), 0)
             #self.neuron_activation = self.neuron_activation.to(x.device)
             #self.neuron_activation = self.neuron_activation.to(mean_activations.device)
